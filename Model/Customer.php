9107da181d4957c1463b0c4ee075b30cb00be439
StefanAmur <stefan.amur22@gmail.com>
2021-11-08 12:05:47 +0100

    Added Customer, CustomerGroup & Product classes

--- Model/Customer.php
+++ Model/Customer.php
@@ -3,13 +3,46 @@
 declare(strict_types=1);
 
 class Customer {
-    private $name;
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private int $groupId;
+    private int $fixedDiscount;
+    private int $variableDiscount;
 
-    public function __construct(string $name) {
-        $this->name = $name;
+
+
+    public function __construct(int $id, string $firstName, string $lastName, int $groupId, int $fixedDiscount, int $variableDiscount) {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->groupId = $groupId;
+        $this->fixedDiscount = $fixedDiscount;
+        $this->variableDiscount = $variableDiscount;
     }
 
-    public function getName(): string {
-        return $this->name;
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getFirstName(): string {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string {
+        return $this->lastName;
+    }
+
+    public function getGroupId(): int {
+        return $this->groupId;
+    }
+
+    public function getFixedDiscount(): int {
+        return $this->fixedDiscount;
+    }
+
+    public function getVariableDiscount(): int {
+        return $this->variableDiscount;
     }
 }
